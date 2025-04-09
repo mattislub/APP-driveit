89ab1066b566b4a1bd04108def45a0299c47f97e
mattislub <166994172+mattislub@users.noreply.github.com>
2025-04-09 18:42:34 +0200

    Update LoginScreen.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, ImageBackground, Image } from 'react-native';
+import { View, StyleSheet, Image, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import TopHeader from '../components/TopHeader';
 import BottomNavBar from '../components/BottomNavBar';
@@ -12,27 +12,27 @@ export default function LoginScreen({ navigation }) {
   return (
     <View style={styles.outerWrapper}>
       <View style={styles.fixedSize}>
-        <View style={styles.background}>
-          {/* תמונה בתחתית */}
-          <Image
-            source={require('../assets/images/new-bg.png')}
-            style={styles.innerImage}
-            resizeMode="contain"
-          />
+        {/* תמונה ברוחב מלא */}
+        <Image
+          source={require('../assets/images/new-bg.png')}
+          style={styles.fullWidthImage}
+          resizeMode="contain"
+        />
 
-          <LinearGradient
-            colors={['#F5F5F5', 'transparent']}
-            style={styles.gradient}
-          />
+        {/* שכבת גרדיאנט על כל המסך */}
+        <LinearGradient
+          colors={['#F5F5F5', 'transparent']}
+          style={styles.fullScreenGradient}
+        />
 
-          <TopHeader
-            onPressMenu={() => handlePress('תפריט')}
-            onPressAction={() => handlePress('חיוב')}
-          />
+        {/* תוכן */}
+        <TopHeader
+          onPressMenu={() => handlePress('תפריט')}
+          onPressAction={() => handlePress('חיוב')}
+        />
 
-          <View style={styles.bottomBar}>
-            <BottomNavBar onPress={(label) => handlePress(label)} />
-          </View>
+        <View style={styles.bottomBar}>
+          <BottomNavBar onPress={(label) => handlePress(label)} />
         </View>
       </View>
     </View>
@@ -51,30 +51,27 @@ const styles = StyleSheet.create({
     height: 640,
     overflow: 'hidden',
     borderRadius: 0,
+    position: 'relative',
   },
-  background: {
-    flex: 1,
-    justifyContent: 'flex-start',
-  },
-  innerImage: {
+  fullWidthImage: {
     width: '100%',
-    height: '50%',
+    height: '100%',
     position: 'absolute',
     bottom: 0,
   },
-  gradient: {
+  fullScreenGradient: {
     position: 'absolute',
     top: 0,
     left: 0,
     right: 0,
-    height: 180,
-    zIndex: 5,
+    bottom: 0,
+    zIndex: 2,
   },
   bottomBar: {
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
-    zIndex: 10,
+    zIndex: 3,
   },
 });
