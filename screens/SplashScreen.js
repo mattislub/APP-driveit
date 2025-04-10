02961af7c4750643c4f2c0baf8102167166bd6a8
mattislub <166994172+mattislub@users.noreply.github.com>
2025-04-10 02:36:27 +0200

    Update SplashScreen.js

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Logo from '../assets/Vector.svg';
 
 export default function SplashScreen() {
+  useEffect(() => {
+    console.log("SplashScreen נטען");
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-      <Text style={{ fontSize: 24 }}>שלום, Splash עובד!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>שלום, Splash עובד!</Text>
+
+      <View style={styles.logoWrapper}>
+        <Logo width={220} height={220} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 30,
+  },
+  logoWrapper: {
+    borderWidth: 2,
+    borderColor: 'red',
+    padding: 10,
+  },
+});
